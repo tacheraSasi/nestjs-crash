4e9cd3a5eda422368d024bd52805696865020106
Tachera Sasi <tacherasasi@gmail.com>
2025-03-08 22:00:48 +0300

    Ellie: more changes on the toys controller

--- src/toys/toys.controller.ts
+++ src/toys/toys.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+
+interface Toy {
+  id: number;
+  name: string;
+  color: string;
+}
 
 @Controller('toys')
-export class ToysController {}
+export class ToysController {
+  private toys = [
+    { id: 1, name: 'Teddy Bear', color: 'brown' },
+    { id: 2, name: 'Doll', color: 'pink' },
+    { id: 3, name: 'Car', color: 'red' },
+  ];
+  
+  @Get()
+  getToys() {
+    return this.toys;
+  }
+  
+  @Post()
+  addToy(@Body() toy:Toy){
+    const newId = this.toys.length + 1;
+    const newToy = { ...toy, id: newId };
+    this.toys.push(newToy);
+    return newToy;
+  }
+}
