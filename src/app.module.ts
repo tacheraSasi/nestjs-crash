675b18f04718fd97a6ca239af1f58df29c2f1f47
Tachera Sasi <tacherasasi@gmail.com>
2025-03-08 21:55:11 +0300

    Ellie: created module toys

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ChicksController } from './chicks/chicks.controller';
 import { ChicksService } from './chicks/chicks.service';
 import { ChicksModule } from './chicks/chicks.module';
+import { ToysModule } from './toys/toys.module';
 
 @Module({
-  imports: [ChicksModule],
+  imports: [ChicksModule, ToysModule],
   controllers: [AppController, ChicksController],
   providers: [AppService, ChicksService],
 })
